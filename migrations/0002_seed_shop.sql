e2a944e166ffef7f6082de05a156f48149eb861d
pejnarin2541-lgtm <pejnarin2541@gmail.com>
2026-09-16 21:34:48 +0700

    Add files via upload

---
+++ migrations/0002_seed_shop.sql
@@ -0,0 +1,2 @@
+INSERT OR IGNORE INTO shops (id,name,branch,owner,phone,pin_hash,plan,status,member_prefix,theme_primary,card_title,card_note,reward_text,created_at)
+VALUES ('shop-006','กาหลง','-','เพชร','0816946145','plain:1111','Trial','active','GGH-7205-','#f17e6f','สะสม 10 แลก 1','สะสมแต้มทุกแก้ว แล้วรับแก้วโปรดฟรีเมื่อครบ 10 แต้ม','แลกชานมไข่มุกฟรี 1 แก้ว',strftime('%s','now')*1000);
